5503fafa196fdaf5cfa700e2495ddf39cf14bfd1
unknown <ihtishamisdeveloper@gmail.com>
2023-12-09 15:29:05 +0500

    portfolio meta changes

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -2,26 +2,26 @@
 @section('title',(!empty(Voyager::setting('site.title')) ? Voyager::setting('site.title') : ''))
 @section('meta')
     <meta name="description"
-          content="Hi, I'm {{ (!empty(Voyager::setting('site.title')) ? Voyager::setting('site.title') : '') }}, a Full-Stack Developer. Expert in app/web development, React Native, WordPress, eCommerce, branding,  logo design, SEO, digital marketing, content writing."/>
-    <link rel="canonical" href="https://ahtesham.me">
+          content="{{ (!empty(Voyager::setting('site.meta_description')) ? Voyager::setting('site.meta_description') : '') }}"/>
+    <link rel="canonical" href="{{ env('APP_URL') }}">
     <meta property="og:locale" content="en_US"/>
     <meta property="og:type" content="portfolio"/>
     <meta property="og:title"
           content="{{ (!empty(Voyager::setting('site.title')) ? Voyager::setting('site.title') : '') }}"/>
     <meta property="og:description"
-          content="Hi, I'm {{ (!empty(Voyager::setting('site.title')) ? Voyager::setting('site.title') : '') }}, a Full-Stack Developer. Expert in app/web development, React Native, WordPress, eCommerce, branding, logo design, SEO, digital marketing, content writing."/>
+          content="{{ (!empty(Voyager::setting('site.meta_description')) ? Voyager::setting('site.meta_description') : '') }}"/>
     <meta property="og:url" content="https://ahtesham.me"/>
     <meta property="og:site_name"
           content="{{ (!empty(Voyager::setting('site.title')) ? Voyager::setting('site.title') : '') }}"/>
-    <meta property="og:image" content="{{ asset('assets/imgs/freelancer/ooo.webp') }}"/>
+    <meta property="og:image" content="{{ Voyager::image(setting('site.favicon')) }}"/>
     <meta property="article:publisher"
-          content="{{ (!empty(Voyager::setting('site.linkedin')) ? Voyager::setting('site.linkedin') : '') }}"/>
+          content="{{ (!empty(Voyager::setting('site.1linkinbio')) ? Voyager::setting('site.1linkinbio') : '') }}"/>
     <meta name="twitter:card" content="summary_large_image"/>
     <meta name="twitter:creator"
           content="{{ (!empty(Voyager::setting('site.title')) ? Voyager::setting('site.title') : '') }}"/>
     <meta name="twitter:site"
           content="{{ (!empty(Voyager::setting('site.title')) ? Voyager::setting('site.title') : '') }}"/>
-    <meta name="twitter:image:src" content="{{ asset('assets/imgs/freelancer/ooo.webp') }}">
+    <meta name="twitter:image:src" content="{{ Voyager::image(setting('site.favicon')) }}">
 @endsection
 @section('content')
     @include('portfolio.inc.autoloader')
